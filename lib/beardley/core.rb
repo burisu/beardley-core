99736c1e63abef7c2ebbde7399fa57db2eac6378
Brice Texier <burisu@oneiros.fr>
2013-04-08 12:05:56 +0200

    Fixes bad scanned path for jars

--- lib/beardley/core.rb
+++ lib/beardley/core.rb
@@ -5,9 +5,7 @@ module Beardley
   module Core
 
     def self.classpath
-      return Dir[File.join(__FILE__, "..", "..", "vendor", "java", "*.jar")].collect{ |path|
-        Pathname.new(path)
-      }
+      return Dir[Pathname.new(__FILE__).dirname.join("..", "..", "vendor", "java", "*.jar")].collect{ |path| Pathname.new(path) }
     end
 
   end
